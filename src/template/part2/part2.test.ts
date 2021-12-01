2e1c69ea2a36e6649b4d7b030cd6c266fc9e93e3
Clare Jolly <clare.jolly@vodafone.com>
2021-12-01 18:13:12 +0000

    refactoring

--- src/template/part2/part2.test.ts
+++ src/template/part2/part2.test.ts
@@ -4,12 +4,12 @@ import { getDataForTest } from '../../../test/unit/utils'
 const { testData, realData } = getDataForTest(__dirname)
 
 describe('part2', () => {
-  // it('returns the expected answer with test data', () => {
-  //   const result = part2(testData)
-  //   expect(result).toStrictEqual()
-  // })
-  // it('returns the expected answer with real data', () => {
-  //   const result = part2(realData)
-  //   expect(result).toStrictEqual()
-  // })
+  it('returns the expected answer with test data', () => {
+    //   const result = part2(testData)
+    //   expect(result).toStrictEqual()
+  })
+  it('returns the expected answer with real data', () => {
+    //   const result = part2(realData)
+    //   expect(result).toStrictEqual()
+  })
 })
