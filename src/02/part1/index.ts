28921d481e9335c91063166da8f2ab895e57dfb8
Clare Jolly <clare.jolly@vodafone.com>
2021-12-02 09:18:46 +0000

    setup day 2

--- src/02/part1/index.ts
+++ src/02/part1/index.ts
@@ -1,3 +1,8 @@
-const part1 = (inputData: string[]) => {}
+const part1 = (inputData: string[]) => {
+  const data = inputData.map(d => d.split(' '))
+  console.log('  ~ file: index.ts ~ line 5 ~ part1 ~ data', data)
+  let x = 0
+  let y = 0
+}
 
 export default part1
