ab07d9b6870e4347aa1a43b0cff13224ee833ac3
Clare Jolly <clare.jolly@vodafone.com>
2021-12-07 09:45:16 +0000

    day 7 setup

---
+++ src/07/part1/index.ts
@@ -0,0 +1,6 @@
+const part1 = (inputData: string[]) => {
+  const data = inputData[0].split(',').map(d => Number(d))
+  console.log('  ~ file: index.ts ~ line 3 ~ part1 ~ data', data)
+}
+
+export default part1
