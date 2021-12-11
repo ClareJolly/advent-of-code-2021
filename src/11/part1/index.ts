26944362419d4b51fad661f6f6ff6cd362c592d3
Clare Jolly <clajollyuk@gmail.com>
2021-12-11 12:09:13 +0000

    day 11 setup

--- src/11/part1/index.ts
+++ src/11/part1/index.ts
@@ -1,7 +1,9 @@
 import { splitStringArrToNumbers } from '../../helpers'
 
 const part1 = (inputData: string[]) => {
-  const data = splitStringArrToNumbers(inputData)
+  const testInputData = ['11111', '19991', '19191', '19991', '11111']
+  const data = splitStringArrToNumbers(testInputData)
+  //   const data = splitStringArrToNumbers(inputData)
   console.log('🚀 ~ file: index.ts ~ line 5 ~ part1 ~ data', data)
 }
 
