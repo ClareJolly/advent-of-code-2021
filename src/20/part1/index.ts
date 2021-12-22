7038b23d8c4ae48f6977d3c3b4acf7f984288fc7
Clare Jolly <clajollyuk@gmail.com>
2021-12-22 17:36:25 +0000

    day 21 part 2 complete

--- src/20/part1/index.ts
+++ src/20/part1/index.ts
@@ -1,3 +1,8 @@
-const part1 = (inputData: string[]) => {}
+import { batchByBlankLines } from 'src/helpers'
+
+const part1 = (inputData: string[]) => {
+  const data = batchByBlankLines(inputData)
+  console.log('🚀 ~ file: index.ts ~ line 6 ~ part1 ~ data', data)
+}
 
 export default part1
